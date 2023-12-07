917830ded9ab0f471efe3e62b435c5a323a7b3e7
bchrysler-fox <135981243+bchrysler-fox@users.noreply.github.com>
2023-12-07 17:19:00 -0600

    Upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.log("This proves the js file worked");
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
